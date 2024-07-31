f05132d95f19444fa937ec9db7065a19a915015f
Ahmet Kaan <ahmet.kaan.yalcinkaya99@gmail.com>
2024-07-31 21:46:59 +0300

    solve conflict merge

--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@
 int main(void){
     std::cout << "Hello, world!" << std::endl;
     std::cout << "Merhaba, dünya!" << std::endl;
+    std::cout << "Γεια σου, κόσμε!" << std::endl;
     return 0;
 }
